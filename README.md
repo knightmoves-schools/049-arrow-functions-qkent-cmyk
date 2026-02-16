701eba883a42cb750bc9084d8025bef2282e8c4e
Dawn DeWaay <99098265+DawnDeWaay@users.noreply.github.com>
2026-02-16 11:43:30 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# 049 Arrow Function
+
+[![049 Arrow Function](https://img.youtube.com/vi/lZW67nR5iXc/0.jpg)](https://www.youtube.com/watch?v=lZW67nR5iXc)
+
+the index.js file
+- should create a variable named add and assign it an arrow function that takes two number parameters and returns their sum
+
+resources:
+- https://www.w3schools.com/js/js_let.asp
+- https://www.w3schools.com/js/js_arrow_function.asp
+
+Copyright &copy; 2023 Knight Moves. All Rights Reserved.
---
+++ test/verify.test.js
@@ -0,0 +1,52 @@
+const http = require("http");
+const fs = require("fs");
+const puppeteer = require("puppeteer");
+const { assert } = require("console");
+
+let server;
+let browser;
+let page;
+
+beforeAll(async () => {
+  server = http.createServer(function (req, res) {
+    fs.readFile(__dirname + "/.." + req.url, function (err, data) {
+      if (err) {
+        res.writeHead(404);
+        res.end(JSON.stringify(err));
+        return;
+      }
+      res.writeHead(200);
+      res.end(data);
+    });
+  });
+
+  server.listen(process.env.PORT || 3000);
+});
+
+afterAll(() => {
+  server.close();
+});
+
+beforeEach(async () => {
+  browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
+
+  page = await browser.newPage();
+  await page.goto("http://localhost:3000/index.html");
+});
+
+afterEach(async () => {
+  await browser.close();
+});
+
+describe('the index.js file', () => {
+  it('should create a variable named `add` and assign it an arrow function that takes two number parameters and returns their sum', async function() {
+      const result = await page.evaluate(() => {
+        return add(1, 2);
+      });
+
+      expect(result).toBe(3);
+  });
+});
+
